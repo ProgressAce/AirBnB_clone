c2628b7f625b9fcdaa5da6ff9fb94cca263b1f00
Ransome-Kuti Omowale <omowaleransomekuti@gmail.com>
2023-07-12 12:14:04 -0700

    Add task 3 files

--- models/base_model.py
+++ models/base_model.py
@@ -1,11 +1,57 @@
 #!/usr/bin/python3
+import uuid
+from datetime import datetime
 """BaseModel
-
 Class definition for BaseModel
-
 """
 
 
 class BaseModel():
-    """BaseModel class"""
-    pass
+    """BaseModel class
+
+    BaseModel defines all common attributes/methods for other classes
+    """
+
+    def __init__(self):
+        """Initializes a BaseModel object
+        """
+        self.id = str(uuid.uuid4())
+        """str: Unique id for each BaseModel"""
+        self.created_at = datetime.now()
+        """datetime: Current datetime when an instance is created"""
+        self.updated_at = self.created_at
+        """datetime: Current datetime when an instance is created,
+        updated everytime the object is changed
+        """
+
+    def __str__(self):
+        """should print: [<class name>] (<self.id>) <self.__dict__>"""
+        return ("[{}] ({}) {}".format(self.__class__.__name__, self.id,
+                self.__dict__))
+
+    def save(self):
+        """Updates the updated_at attribute with the current datetime
+        """
+
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """returns a dictionary of all keys/values of __dict__ of the instance
+
+        a key __class__ is added to this dictionary with the
+        class name of the object
+
+        created_at and updated_at are converted to string object in ISO format:
+        format: %Y-%m-%dT%H:%M:%S.%f (ex: 2017-06-14T22:31:03.285259
+        """
+
+        new_dict = {key: self.__dict__[key] for key in self.__dict__}
+        new_dict["__class__"] = self.__class__.__name__
+        new_dict["created_at"] = self.created_at.isoformat()
+        new_dict["updated_at"] = self.updated_at.isoformat()
+        return new_dict
+
+
+if __name__ == "__main__":
+    b1 = BaseModel()
+    print(b1)
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import unittest
+from datetime import datetime
 from models.base_model import BaseModel
 """Tests the BaseModel class"""
 
@@ -8,17 +9,46 @@ class TestBaseModel(unittest.TestCase):
     """Unittest class for BaseModel"""
 
     def setUp(self):
-        """SetUp for this tests"""
-        self.model = BaseModel()
+        """SetUp at the start of each test"""
+        self.base1 = BaseModel()
+        self.base2 = BaseModel()
+        # self.base1.id = "base1"
+        # self.base2.id = "base2"
 
     def test_isinstance(self):
         """Test instace of BaseModel"""
-        self.assertIsInstance(self.model, BaseModel)
+        self.assertIsInstance(self.base1, BaseModel)
 
-    def test_attribute(self):
-        """Test instance attributes"""
-        with self.assertRaises(AttributeError):
-            self.model.gg
+    def test_id(self):
+        """Test the id instance attributes"""
+        self.assertIn("id", self.base1.__dict__)
+        self.assertIs(type(self.base1.id), str)
+        self.assertNotEqual(self.base1.id, self.base2.id)
+
+    def test_created_at(self):
+        """Test the created_at instance attributes"""
+        self.assertIn("created_at", self.base1.__dict__)
+        self.assertIs(type(self.base1.created_at), datetime)
+
+    def test_updated_at(self):
+        """Test the updated_at instance attributes"""
+        self.assertIn("updated_at", self.base1.__dict__)
+        self.assertIs(type(self.base1.updated_at), datetime)
+
+    def test_save_method(self):
+        """Test that the method updates the created_at attribute"""
+        up1 = self.base2.updated_at
+        self.base2.save()
+        up2 = self.base2.updated_at
+        self.assertNotEqual(up1, up2)
+
+    def test_to_dict_method(self):
+        """Test that the to_dict method returns a dictionary
+            that has all the required key value pairs"""
+        dict1 = self.base2.to_dict()
+        self.assertEqual(dict1["__class__"], "BaseModel")
+        self.assertIs(type(dict1["created_at"]), str)
+        self.assertIs(type(dict1["updated_at"]), str)
 
 
 if __name__ == "__main__":
