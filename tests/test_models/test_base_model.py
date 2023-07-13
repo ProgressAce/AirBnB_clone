720ffbc53badaaca4f391755bdedd06d753cc359
SubtleDaiyaan <daiyaansallie7@gmail.com>
2023-07-13 02:53:15 -0700

    test: add tests for new objects_with_kwargs feature

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -50,6 +50,43 @@ class TestBaseModel(unittest.TestCase):
         self.assertIs(type(dict1["created_at"]), str)
         self.assertIs(type(dict1["updated_at"]), str)
 
+    # Task 4 - kwargs argument
+    def test_kwargs_empty(self):
+        """Test that the base attributes are created when kwargs is empty.
+
+        'id' and 'created_at' attributes are created."""
+
+        self.assertTrue(hasattr(self.base1, 'id'))
+        self.assertTrue(hasattr(self.base1, 'created_at'))
+        self.assertTrue(hasattr(self.base1, 'updated_at'))
+
+    def test_kwargs_not_empty(self):
+        """Test that attributes are created using all the keys from kwargs
+
+        This allows a new instance to be created using a previous instance's
+        attributes.
+
+        __class__ should not be one of these attributes and
+        'created_at' and 'updated_at' should be datetime objects."""
+
+        self.json_dict = {
+                'id': '56d43177-cc5f-4d6c-a0c1-e167f8c27337',
+                'created_at': '2017-09-28T21:03:54.052298',
+                'updated_at': '2017-09-28T21:03:54.052302',
+                'name': 'My_First_Model',
+                '__class__': 'BaseModel'}
+        self.new_base = BaseModel(**self.json_dict)
+
+        # check that attributes were created from all the keys
+        self.assertFalse('__class__' in self.new_base.__dict__)
+        for key in self.json_dict.keys():
+            if key == '__class__':
+                continue
+            self.assertTrue(hasattr(self.new_base, key))
+
+        # check for datetime objects
+        self.assertEqual(type(self.new_base.created_at), datetime)
+        self.assertEqual(type(self.new_base.updated_at), datetime)
 
 if __name__ == "__main__":
     unittest.main()
