51dfeeace4471d8db354ce0625a9d14db9cc4ce1
SubtleDaiyaan <daiyaansallie7@gmail.com>
2023-07-15 15:05:36 -0700

    test: add test file for user class

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Unittests for the User class."""
+import unittest
+from models.user import User
+from models.base_model import BaseModel
+
+
+class TestUser(unittest.TestCase):
+    """Unittests for the User class."""
+    pass
