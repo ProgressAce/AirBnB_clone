6c6913ec6875762b52ac15900474be9da09bec72
Ransome-Kuti Omowale <omowaleransomekuti@gmail.com>
2023-07-17 14:19:55 +0000

    Update console, new classes added as comments

--- console.py
+++ console.py
@@ -7,8 +7,15 @@ import cmd
 import models
 from models.base_model import BaseModel
 from models.user import User
+# from models.state import State
+# from models.city import City
+# from models.amenity import Amenity
+# from models.place import Place
+# from models.review import Review
 all_objs = models.storage.all()
 """Dictionary of all objects"""
+# classes = {'BaseModel': BaseModel, 'User': User, 'State': State, 'City': City,
+#           'Amenity': Amenity, 'Place': Place, 'Review': Review}
 classes = {'BaseModel': BaseModel, 'User': User}
 """Dictionary of all model classes"""
 
