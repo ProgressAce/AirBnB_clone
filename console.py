7329f0c71269bb7a6fff81eb9571c136978ce4af
Ransome-Kuti Omowale <omowaleransomekuti@gmail.com>
2023-07-17 00:43:55 +0000

    Update console to handle User objects

--- console.py
+++ console.py
@@ -6,7 +6,11 @@ The command line interpreter for our AirBnB clone
 import cmd
 import models
 from models.base_model import BaseModel
+from models.user import User
 all_objs = models.storage.all()
+"""Dictionary of all objects"""
+classes = {'BaseModel': BaseModel, 'User': User}
+"""Dictionary of all model classes"""
 
 
 class HBNBCommand(cmd.Cmd):
@@ -36,41 +40,44 @@ class HBNBCommand(cmd.Cmd):
     def do_create(self, arg):
         """Creates a new instance of BaseModel
         saves it (to the JSON file) and prints the id.
-        Ex: $ create BaseModel
+        Ex: $ create BaseModel or $ create User
         """
         if not arg:
             print("** class name missing **")
-        elif arg == "BaseModel":
-            new_base = BaseModel()
-            new_base.save()
-            print(new_base.id)
+        elif arg in classes:
+            new_obj = classes[arg]()
+            new_obj.save()
+            print(new_obj.id)
         else:
             print("** class doesn't exist **")
 
     def help_create(self):
-        print(''.join([
-            'Creates a new instance of BaseModel, ',
-            'saves it (to the JSON file) and prints the id.\n',
-            '\nExample:\n',
-            '\n$ create BaseModeli\n'
+        print('\n'.join([
+            'Creates a new instance of BaseModel,',
+            'saves it (to the JSON file) and prints the id.',
+            'Example:',
+            '$ create BaseModel',
+            'or',
+            '$ create User'
         ]))
 
     def do_show(self, arg):
         """Prints the string representation of an instance
         based on the class name and id.
         Ex: $ show BaseModel 1234-1234-1234
+        Ex: $ show User 1234-1234-1234
         """
         arg_list = arg.split()
         if not arg_list:
             print("** class name missing **")
-        elif arg_list[0] != "BaseModel":
+        elif arg_list[0] not in classes:
             print("class doesn't exist")
         elif len(arg_list) == 1:
             print("** instance id missing *")
-        elif f'{"BaseModel"}.{arg_list[1]}' not in all_objs:
+        elif f'{arg_list[0]}.{arg_list[1]}' not in all_objs:
             print("** no instance found **")
         else:
-            key = f'{"BaseModel"}.{arg_list[1]}'
+            key = f'{arg_list[0]}.{arg_list[1]}'
             print(all_objs[key])
 
     def do_destroy(self, arg):
@@ -82,7 +89,7 @@ class HBNBCommand(cmd.Cmd):
         arg_list = arg.split()
         if not arg_list:
             print("** class name missing **")
-        elif arg_list[0] != "BaseModel":
+        elif arg_list[0] not in classes:
             print("class doesn't exist")
         elif len(arg_list) == 1:
             print("** instance id missing *")
@@ -98,16 +105,16 @@ class HBNBCommand(cmd.Cmd):
         based or not on the class name.
         The printed result is a list of strings
 
-        Ex: $ all BaseModel or $ all.
+        Ex: $ all BaseModel or $ User or $ all.
         """
         arg_list = arg.split()
         if not arg_list:
             all_list = [str(all_objs[key]) for key in all_objs]
             print(all_list)
-        elif arg_list[0] != "BaseModel":
+        elif arg_list[0] not in classes:
             print("** class doesn't exist *")
         else:
-            all_list = [str(all_objs[key]) for key in all_objs if "BaseModel"
+            all_list = [str(all_objs[key]) for key in all_objs if arg_list[0]
                         in key]
             print(all_list)
 
@@ -118,11 +125,12 @@ class HBNBCommand(cmd.Cmd):
         Usage: update <class name> <id> <attribute name> "<attribute value>"
 
         Ex: $ update BaseModel 1234-1234-1234 email "aibnb@mail.com"
+        Ex: $ update User 1234-1234-1234 email "aibnb@mail.com"
         """
         arg_list = arg.split()
         if not arg_list:
             print("** class name missing **")
-        elif arg_list[0] != "BaseModel":
+        elif arg_list[0] not in classes:
             print("class doesn't exist")
         elif len(arg_list) == 1:
             print("** instance id missing *")
@@ -134,12 +142,18 @@ class HBNBCommand(cmd.Cmd):
             print("** value missing *")
         else:
             key = f'{arg_list[0]}.{arg_list[1]}'
-            print(all_objs[key].updated_at)
             value = arg_list[3].strip('"')
             att_name = arg_list[2]
-            all_objs[key].__dict__[att_name] = value
-            all_objs[key].save()
-            print(all_objs[key].updated_at)
+            if att_name in all_objs[key].__dict__:
+                try:
+                    value = type(all_objs[key].__dict__[att_name])(value)
+                    all_objs[key].__dict__[att_name] = value
+                    all_objs[key].save()
+                except ValueError:
+                    pass
+            else:
+                all_objs[key].__dict__[att_name] = value
+                all_objs[key].save()
 
 
 if __name__ == '__main__':
